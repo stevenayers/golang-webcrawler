3f8eba3b63249ecf2aa9b2808f3c2474123f9513
Steven Ayers <sayers@equalexperts.com>
2019-03-18 12:13:26 +0000

    Merge FetchUrls & FetchPage so docError and httpError return you out of function as expected, preventing a nil pointer reference upstream.
    Use regex matching for URLs in IsRelativeUrl (isInternalLink).
    
    Trailing slash removal in ParseRelativeUrl (parseURI).
    
    Refactoring choice of wording and rationalise with comments.

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -1,3 +1,6 @@
+/*
+Fetches page data, converts the HTML into Urls, and formats the URLs
+*/
 package fetcher
 
 import (
@@ -5,59 +8,58 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"path"
+	"regexp"
 	"strings"
 )
 
 type Page struct {
-	URL   *url.URL
+	Url   *url.URL
 	Depth int
 	Links []*Page
 }
 
-func FetchURLs(currentURL *url.URL) (urls []*url.URL) {
-	doc := fetchPage(currentURL)
-	localProcessed := make(map[string]struct{})
+func FetchUrls(currentUrl *url.URL) (urls []*url.URL, httpError error, docError error) {
+	resp, httpError := http.Get(currentUrl.String())
+	if httpError != nil {
+		log.Printf("failed to get URL %s: %v", currentUrl.String(), httpError)
+		return
+	}
+	if !strings.HasPrefix(resp.Header.Get("Content-Type"), "text/html") { // Check if HTML file
+		return
+	}
+	defer resp.Body.Close() // Closes response body FetchUrls function is done.
+	doc, docError := goquery.NewDocumentFromReader(resp.Body)
+	if docError != nil {
+		log.Printf("failed to parse HTML: %v", docError)
+		return
+	}
+	localProcessed := make(map[string]struct{}) // Ensures we don't store the same Url twice and
+	// end up spawning 2 goroutines for same result
 	doc.Find("a").Each(func(index int, item *goquery.Selection) {
 		href, ok := item.Attr("href")
-		if isInternalLink(href) && ok && href != "" {
-			uri := parseURI(currentURL, href)
-			_, isPresent := localProcessed[uri.Path]
+		if IsRelativeUrl(href) && ok && href != "" {
+			absoluteUrl := ParseRelativeUrl(currentUrl, strings.TrimRight(href, "/")) // Standardises URL
+			_, isPresent := localProcessed[absoluteUrl.Path]
 			if !isPresent {
-				localProcessed[uri.Path] = struct{}{}
-				urls = append(urls, uri)
+				localProcessed[absoluteUrl.Path] = struct{}{}
+				urls = append(urls, absoluteUrl)
 			}
 		}
 	})
 	return
 }
 
-func fetchPage(currentURL *url.URL) (doc *goquery.Document) {
-	resp, err := http.Get(currentURL.String())
-	if err != nil {
-		log.Printf("failed to get URL %s: %v", currentURL.String(), err)
-		return
-	}
-	defer resp.Body.Close()
-	if !strings.HasPrefix(resp.Header.Get("Content-Type"), "text/html") {
-		return
-	}
-	doc, err = goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		log.Printf("failed to parse HTML: %v", err)
-		return
-	}
-	return
-}
-
-func parseURI(rootURL *url.URL, uri string) (absoluteURL *url.URL) {
-	absoluteURL, err := url.Parse(rootURL.Scheme + "://" + rootURL.Host + uri)
+func ParseRelativeUrl(rootUrl *url.URL, relativeUrl string) (absoluteUrl *url.URL) {
+	absoluteUrl, err := url.Parse(rootUrl.Scheme + "://" + rootUrl.Host + path.Clean("/"+relativeUrl))
 	if err != nil {
 		return nil
 	}
-	absoluteURL.Fragment = ""
+	absoluteUrl.Fragment = "" // Removes '#' identifiers from Url
 	return
 }
 
-func isInternalLink(href string) bool {
-	return !strings.Contains(href, "http") && strings.HasPrefix(href, "/")
+func IsRelativeUrl(href string) bool {
+	match, _ := regexp.MatchString("^(?:[a-zA-Z]+:)?//", href)
+	return !match
 }
